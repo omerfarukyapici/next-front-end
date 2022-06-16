d526e92384f7881821fc82cbcc93f084a7114bee
omerfarukyapici <omerfarukyapici20@gmail.com>
2022-06-17 00:31:27 +0300

    Changed HomePage.js component background color

--- pages/components/HomePage.js
+++ pages/components/HomePage.js
@@ -1,6 +1,6 @@
 const HomePage = () => {
     return (
-        <div className="bg-[yellow]">            
+        <div className="bg-[blue]">            
             HomePage
         </div>
     )
