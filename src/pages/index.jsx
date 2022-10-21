261e5229e3778c8ab21f36427ab75f63aa7f6099
omerfarukyapici <omerfarukyapici20@gmail.com>
2022-10-21 16:49:14 +0300

    Prism.js fix 2

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import Footer from "../components/Footer"
 import Header from "../components/Header"
 import { useEffect, useState } from "react";
 import Prism from "prismjs";
-import "prismjs/themes/prismholitheme.css";
+import "prismjs/themes/prism-dark.css";
 
 
 /* 
